3e7720125c354ccdf46eb51e3617002557f615db
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-01-18 22:59:46 +0100

    number of records

--- src/ExhibitorsList.js
+++ src/ExhibitorsList.js
@@ -1,4 +1,4 @@
-
+import {useEffect} from 'react'
 import Exhibitor from './components/Exhibitor'
 import { useExhibitorsListContext } from './ExhibitorsListContext';
 import {processArrayData, isEmpty} from 'eventjuicer-site-components'
@@ -6,20 +6,26 @@ import ExhibitorContext from './ExhibitorContext'
 import ExhibitorRealAssignments from './components/ExhibitorRealAssignments';
 import ExhibitorProfileErrors from './components/ExhibitorProfileErrors';
 import ExhibitorPurchases from './components/ExhibitorPurchases';
-
+import NumberOfRecords from './components/NumberOfRecords';
 
 const ExhibitorList = ({roles}) => {
 
-    const {data, account, filtered, sort, flags} = useExhibitorsListContext()
+    const {data, account, filtered, sort, flags, setFlags} = useExhibitorsListContext()
     const finalData = !isEmpty(account) ? data: filtered
     const filter = !isEmpty(account)? (item)=>item.account == account: null
+    const items = processArrayData(finalData, {sort, filter})
 
-    return processArrayData(finalData, {sort, filter}).map(({id}) => <ExhibitorContext  key={id} id={id}>
+    useEffect(()=>{
+        //remember flags state!
+    },[filtered.length])
+
+    return (<div>
+       <NumberOfRecords count={items.length} />{items.map(({id}) => <ExhibitorContext  key={id} id={id}>
        <Exhibitor />
        <ExhibitorProfileErrors active={flags.assessment} />
        <ExhibitorRealAssignments active={flags.assignment} />
        <ExhibitorPurchases roles={roles} active={flags.assignment} />
-      </ExhibitorContext>)
+      </ExhibitorContext>)}</div>)
 
 
 }
