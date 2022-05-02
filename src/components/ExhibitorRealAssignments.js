716c12e02f54884308a2d31c992c2cd38e8835ae
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-02 23:01:37 +0200

    Update ExhibitorRealAssignments.js

--- src/components/ExhibitorRealAssignments.js
+++ src/components/ExhibitorRealAssignments.js
@@ -1,5 +1,5 @@
 
-import { Alert, makeStyles, classNames } from 'eventjuicer-site-components';
+import { Alert, makeStyles, classNames, useSettings } from 'eventjuicer-site-components';
 import {servicesRealAssignments} from './helpers'
 import {useExhibitorContext} from './ExhibitorContext'
 import {red, teal} from '@material-ui/core/colors'
@@ -29,25 +29,33 @@ const useStyles = makeStyles(theme => ({
 
 }))
 
+const defaultProps = {
+    show_vouchers: true, 
+    show_furniture: true
+}
 
-const ExhibitorDetails = () => {
+
+const ExhibitorRealAssignments = (props) => {
 
     const classes = useStyles()
     const {data:{reps, purchases, profile}} = useExhibitorContext()
     const {carpet_color} = profile
-
+    const settings = useSettings("aggregates", {})
+    const { show_vouchers, show_furniture } = Object.assign({}, defaultProps, settings, props)
     const {catering, parking, tables, chairs} = servicesRealAssignments(purchases, reps)
     
     return (<span>
-            <span className={classNames(classes.assignment, classes.welcomepacks )}>
-                <span>Cateringowe <strong>{catering}</strong></span>
-                <span>Parkingowe <strong>{parking}</strong></span> 
-            </span>
-            <span className={classNames(classes.assignment, classes.event )}>
-                <span>Stoły <strong>{tables}</strong></span> 
-                <span>Krzesła <strong>{chairs}</strong></span> 
-               
-            </span>
+            
+            {show_vouchers ? <span className={classNames(classes.assignment, classes.welcomepacks )}>
+                <span>Catering <strong>{catering}</strong></span>
+                <span>Parking <strong>{parking}</strong></span> 
+            </span>: null}
+            
+            {show_furniture? <span className={classNames(classes.assignment, classes.event )}>
+                <span>Tables <strong>{tables}</strong></span> 
+                <span>Chairs <strong>{chairs}</strong></span> 
+            </span>: null}
+
             {carpet_color && <span className={classNames(classes.assignment )}>
             {carpet_color}
             </span>}
@@ -56,4 +64,4 @@ const ExhibitorDetails = () => {
 }
 
 
-export default ExhibitorDetails
+export default ExhibitorRealAssignments
